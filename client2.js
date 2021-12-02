180be5cfc2f3d6b75204c7b6ac29320caa28814b
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-12-02 00:46:47 +0000

    Learned the how to send the server commands for the client snake

--- client2.js
+++ client2.js
@@ -16,6 +16,13 @@ const connect = () => {
   conn.on('connect', () => {
     console.log('Your snake is in the game!');
     conn.write("Name: KEV");
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 100);
+    setInterval(() => {
+      conn.write("Move: up");
+    }, 500);
+
   })
   return conn;
 };
